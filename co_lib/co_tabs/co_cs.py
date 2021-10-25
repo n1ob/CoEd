d119baa79bc9013c03fc8c508174dd35b1d66265
mopy <rti@gmx.de>
2021-10-25 07:09:12 +0200

    copyright

--- co_lib/co_tabs/co_cs.py
+++ co_lib/co_tabs/co_cs.py
@@ -1,3 +1,18 @@
+# ***************************************************************************
+# *   Copyright (c) 2021 n1ob <niob@gmx.com>                                *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# ***************************************************************************
 from typing import List, Dict
 
 import FreeCAD as App
